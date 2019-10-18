c7134367af3508a80c0d22172ea41447d5b2abfb
Charlie Oster <oster.charlie@yahoo.com>
2019-10-18 00:41:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -73,6 +73,71 @@ end
   end	  end
 end	end
 
+let!(:betty) { Author.new("Betty") } 
 
+
+    describe "#new" do 	  describe "#new" do 
+      it "is initialized with a name" do 	    it "is initialized with a name" do 
+        expect{Author.new("Betty")}.to_not raise_error	      expect{Author.new("Betty")}.to_not raise_error
+      end	    end
+
+
+      it "is initialized with an empty collection of posts" do 	    it "is initialized with an empty collection of posts" do 
+        expect(betty.instance_variable_get(:@posts)).to match([])	      expect(betty.instance_variable_get(:@posts)).to match([])
+      end	
+    end	    end
+  end
+
+
+    describe "#name" do	  describe "#name" do
+      it "has an attr_accessor for name" do 	    it "has an attr_accessor for name" do 
+        expect(betty.name).to eq("Betty")	      expect(betty.name).to eq("Betty")
+    end	    end
+  end
+
+
+    describe "#posts" do 	  describe "#posts" do 
+      it "has many posts" do	    it "has many posts" do
+        expect(betty.posts).to be_a(Array)	      expect(betty.posts).to be_a(Array)
+      end	
+    end	    end
+  end
+
+
+    describe "#add_post" do 	  describe "#add_post" do 
+      it "takes in an argument of a post and adds that post to the author's collection and tells the post that it belongs to that author" do 	    it "takes in an argument of a post and adds that post to the author's collection and tells the post that it belongs to that author" do 
+        hello_world = Post.new("Hello World")	      hello_world = Post.new("Hello World")
+        betty.add_post(hello_world)	      betty.add_post(hello_world)
+        expect(betty.posts).to include(hello_world)	      expect(betty.posts).to include(hello_world)
+        expect(hello_world.author).to eq(betty)	      expect(hello_world.author).to eq(betty)
+      end	
+    end	    end
+  end
+
+
+    describe "#add_post_by_title" do 	  describe "#add_post_by_title" do 
+      it "takes in an argument of a post title, creates a new post with it and associates the post and author" do 	    it "takes in an argument of a post title, creates a new post with it and associates the post and author" do 
+        betty.add_post_by_title("My Great Blog Post")	      betty.add_post_by_title("My Great Blog Post")
+        # binding.pry	      # binding.pry
+        expect(betty.posts.last.title).to eq("My Great Blog Post")	      expect(betty.posts.last.title).to eq("My Great Blog Post")
+        expect(betty.posts.last.author).to eq(betty)	      expect(betty.posts.last.author).to eq(betty)
+      end	
+    end	    end
+  end
+
+
+    describe ".post_count" do 	  describe ".post_count" do 
+      it "is a class method that returns the total number of posts associated to all existing authors" do 	    it "is a class method that returns the total number of posts associated to all existing authors" do 
+        # binding.pry	      # binding.pry
+        expect(Author.post_count).to eq(2)	      expect(Author.post_count).to eq(2)
+      end	    end
+
+
+      it "uses the class variable, @@post_count" do 	    it "uses the class variable, @@post_count" do 
+        expect(Author.class_variable_get(:@@post_count)).to be_a(Integer)	      expect(Author.class_variable_get(:@@post_count)).to be_a(Integer)
+      end	
+    end	    end
+  end	  end
+end 	end 
   
     
